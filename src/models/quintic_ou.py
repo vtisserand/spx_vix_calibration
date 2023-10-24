186d2f959bc6a851799a9e67528d07ec55604185
Valent11111 <valent.3.1415@gmail.com>
2023-10-24 13:52:35 +0200

    Implemented underlying trajectories

--- src/models/quintic_ou.py
+++ src/models/quintic_ou.py
@@ -16,10 +16,12 @@ class QuinticOU(BaseModel):
         self.a_k = None
         self.t_array_nodes = None
         self.fv_nodes = None
-        self.spine_order = None
+        self.rho = None
+        self.S0 = None
+        self.spline_order = None
 
 
-    def set_parameters(self, eps: float, H: float, a_k: list, t_array_nodes: list, fv_nodes: list, spine_order: int = 3):
+    def set_parameters(self, eps: float, H: float, a_k: list, t_array_nodes: list, fv_nodes: list, rho: float, S0: float, spline_order: int = 3):
         self.eps = eps
         self.H = H
         self.eta_tild = self.eps**(self.H-0.5)
@@ -28,7 +30,9 @@ class QuinticOU(BaseModel):
         self.a_k = np.array([a_0, a_1, 0, a_3, 0, a_5])
         self.t_array_nodes = np.array(t_array_nodes)
         self.fv_nodes = np.array(fv_nodes)
-        self.spine_order = spine_order
+        self.rho = rho
+        self.S0 = S0
+        self.spline_order = spline_order
 
     def fit(self, data):
         pass
@@ -47,14 +51,17 @@ class QuinticOU(BaseModel):
             result = result * x + poly[i].reshape(-1, 1)
         return result
 
-    def generate_ou(self, num_steps: int, tt: np.ndarray, num_sims: int = 1):
+    def generate_brownian_trajectories(self, num_steps: int, num_sims: int = 1):
+        w1 = np.random.normal(0, 1, (num_steps, num_sims))
+        return w1
+
+
+    def generate_ou_trajectories(self, tt: np.ndarray, w1: np.ndarray):
         """
         Generate an Ornstein-Uhlenbeck trajectory according to the following scheme:
         """
         # Brownian path
-        w1_orig = np.random.normal(0, 1, (num_steps, num_sims))
-        w1 = np.concatenate(
-            (np.zeros(w1_orig.shape[1])[np.newaxis, :], w1_orig))
+        w1 = np.concatenate((np.zeros(w1.shape[1])[np.newaxis, :], w1))
 
         exp1 = np.exp(self.kappa_tild*tt)
         exp2 = np.exp(2*self.kappa_tild*tt)
@@ -67,7 +74,7 @@ class QuinticOU(BaseModel):
         Xt = np.array(X[:-1])
         return Xt
 
-    def generate_trajectory(self, tt: np.ndarray, Xt: np.ndarray):
+    def generate_vol_trajectory(self, tt: np.ndarray, Xt: np.ndarray):
         """
         Generates volatility trajectories according to the quintic OU model.
         """
@@ -82,34 +89,54 @@ class QuinticOU(BaseModel):
         normal_var = normal_var[1:].reshape(-1,1)
 
         polynomial = self.compute_polynomial(self.a_k[::-1], len(self.a_k), Xt)
-        fv_var_curve_spline_sqrt = interpolate.splrep(self.t_array_nodes, np.sqrt(self.fv_nodes), k=self.spine_order)
+        fv_var_curve_spline_sqrt = interpolate.splrep(self.t_array_nodes, np.sqrt(self.fv_nodes), k=self.spline_order)
         fv_curve = (interpolate.splev(tt, fv_var_curve_spline_sqrt, der=0)) ** 2
         fv_curve = fv_curve[:-1].reshape(-1,1)
         volatility = np.sqrt(fv_curve) * (polynomial / np.sqrt(normal_var))
         return volatility
 
+    def generate_underlying_trajectories(self, time_step: int, w1: np.ndarray, volatility: np.ndarray):
+        logS0 = np.log(self.S0)
+        logSt = logS0 + np.cumsum(- 0.5 * time_step * (volatility * self.rho) ** 2 + np.sqrt(time_step) * self.rho * volatility * w1, axis=0)
+        logSt = np.concatenate((np.full(logSt.shape[1], logS0)[np.newaxis, :], logSt))
+        logSt = np.array(logSt[:-1])
+        St = np.exp(logSt)
+        return St
+
     def plot_trajectories(self, num_steps: int, time_step: int, num_sims: int = 1):
         tt = self.get_dates(num_steps, time_step)
-        Xt = self.generate_ou(num_steps, tt, num_sims)
-        volatility = self.generate_trajectory(tt, Xt)
+        w1 = self.generate_brownian_trajectories(num_steps, num_sims)
+
+        Xt = self.generate_ou_trajectories(tt, w1)
+        volatility = self.generate_vol_trajectory(tt, Xt)
+        St = self.generate_underlying_trajectories(time_step, w1, volatility)
+
         tt = tt[:-1]
 
-        fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(9, 6))
+        # Plot OU trajectories
+        fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(9, 8))
         for i in range(num_sims):
             axs[0].plot(tt, Xt[:, i])
         plt.sca(axs[0])
         axs[0].set_ylabel("OU Simulations")
         axs[0].set_title(
             "Simulated OU Processes (eps={}, H={}, num_sims={})".format(np.round(self.eps, decimals=3), self.H, num_sims))
+        # Plot volatility trajectories
         for i in range(num_sims):
             axs[1].plot(tt, volatility[:, i])
-        plt.sca(axs[0])
+        plt.sca(axs[1])
         axs[1].set_ylabel("Volatility")
         axs[1].set_title("Simulated Volatility Paths (num_sims={})".format(num_sims))
+        # Plot underlying trajectories
+        for i in range(num_sims):
+            axs[2].plot(tt, St[:, i])
+        plt.sca(axs[2])
+        axs[2].set_ylabel("Underlying")
+        axs[2].set_title("Simulated Underlying Paths (num_sims={})".format(num_sims))
 
-        plt.subplots_adjust(top=0.78)
+        plt.subplots_adjust(top=0.8)
         fig.suptitle("Quintic OU Model Simulations", fontsize=22, y=0.943)
-        fig.subplots_adjust(hspace=0.4)
+        fig.subplots_adjust(hspace=0.5)
         # plt.savefig("Quintic_OU_simulations.pdf")
         plt.show()
 
--- src/models/test.py
+++ src/models/test.py
@@ -11,11 +11,16 @@ t_array_nodes = [0, 0.03, 1 / 12, 2 / 12, 3 / 12, 6 / 12, 12 / 12, 24 / 12]
 fv_nodes = list(np.ones_like(t_array_nodes) * 0.02)  # fix fwd variance at 0.02
 #fv_nodes = np.array([0.012,0.018,0.035,0.026,0.027,0.019,0.025,0.025])
 
+rho = -0.65
+S0 = 100
+
 quintic_ou = QuinticOU()
-quintic_ou.set_parameters(eps, H, a_k, t_array_nodes, fv_nodes)
+quintic_ou.set_parameters(eps, H, a_k, t_array_nodes, fv_nodes, rho, S0)
 
 num_steps = 400
 time_step = (1 / 12) / num_steps
+#num_steps = 400 * 12 * 10
+time_step = 1 / num_steps
 num_sims = 10
 
 tt = quintic_ou.get_dates(num_steps, time_step)
