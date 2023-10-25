287f0e3c6e7d825b42f04fe07e05aa8677dd813d
Vivien Tisserand <vivien.tisserand@gmail.com>
2023-10-25 13:21:03 +0200

    Ruff formatter

--- src/models/test.py
+++ src/models/test.py
@@ -1,15 +1,13 @@
-
-
 import numpy as np
 from quintic_ou import QuinticOU
 
 
 eps = 1 / 52
 H = 0
-a_k = [0.01,1,0.214, 0.227]
+a_k = [0.01, 1, 0.214, 0.227]
 t_array_nodes = [0, 0.03, 1 / 12, 2 / 12, 3 / 12, 6 / 12, 12 / 12, 24 / 12]
 fv_nodes = list(np.ones_like(t_array_nodes) * 0.02)  # fix fwd variance at 0.02
-#fv_nodes = np.array([0.012,0.018,0.035,0.026,0.027,0.019,0.025,0.025])
+# fv_nodes = np.array([0.012,0.018,0.035,0.026,0.027,0.019,0.025,0.025])
 
 rho = -0.65
 S0 = 100
@@ -19,7 +17,7 @@ quintic_ou.set_parameters(eps, H, a_k, t_array_nodes, fv_nodes, rho, S0)
 
 num_steps = 400
 time_step = (1 / 12) / num_steps
-#num_steps = 400 * 12 * 10
+# num_steps = 400 * 12 * 10
 time_step = 1 / num_steps
 num_sims = 10
 
