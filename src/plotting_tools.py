7e637a088d8753032ccce0d95ca0c7f05fd07221
Valent11111 <valent.3.1415@gmail.com>
2023-11-28 23:05:07 +0100

    Added comments in plotting_tools

--- src/plotting_tools.py
+++ src/plotting_tools.py
@@ -27,8 +27,21 @@ class AutocorrelationTools:
     def __init__(self, df):
         self.df = df.copy()
 
+
     @staticmethod
     def plot_autocorrelations_without_pacf(arr_returns, nlags, confidence_level, title):
+        """
+        Plot the ACF of the time series given as input.
+
+        Parameters:
+        - arr_returns (list or numpy.ndarray): Time series
+        - nlags (int): The maximum lag to be displayed
+        - confidence_level (float): The confidence level for the confidence interval of the autocorrelations
+        - title (string): The title of the figure.
+
+        Returns:
+        - None
+        """
         arr_acf = sm.tsa.stattools.acf(arr_returns, nlags=nlags, alpha=None)
         arr_acf = arr_acf[1:]
         x = np.array(range(1, nlags+1))
@@ -63,8 +76,21 @@ class AutocorrelationTools:
         fig.subplots_adjust(hspace=0.2, top=0.75)
         plt.show()
     
+
     @staticmethod
     def plot_autocorrelations_with_pacf(arr_returns, nlags, confidence_level, title):
+        """
+        Plot the ACF and PACF of the time series given as input.
+
+        Parameters:
+        - arr_returns (list or numpy.ndarray): Time series
+        - nlags (int): The maximum lag to be displayed
+        - confidence_level (float): The confidence level for the confidence interval of the autocorrelations
+        - title (string): The title of the figure.
+
+        Returns:
+        - None
+        """
         arr_acf = sm.tsa.stattools.acf(arr_returns, nlags=nlags, alpha=None)
         arr_pacf = sm.tsa.stattools.pacf(arr_returns, nlags=nlags, method='ywadjusted', alpha=None)
         arr_acf = arr_acf[1:]
@@ -111,7 +137,22 @@ class AutocorrelationTools:
         fig.subplots_adjust(hspace=0.2, top=0.85)
         plt.show()
 
+
     def plot_autocorrelations(self, start_date, end_date, with_pacf, nlags=20, confidence_level=0.95, title="Autocorrelations"):
+        """
+        Construct the series of returns and plot the autocorrelations
+
+        Parameters:
+        - start_date (datetime.datetime): The start date to be considered when calculating the autocorrelations
+        - end_date (datetime.datetime): The end date to be considered when calculating the autocorrelations
+        - with_pacf (bool): A boolean value equal to True if the PACF needs to be displayed, otherwise False
+        - nlags (int): The maximum lag to be displayed. Default is 20
+        - confidence_level (float): The confidence level for the confidence interval of the autocorrelations. Default is 0.95
+        - title (string): The title of the figure. Default is 'Autocorrelations'
+
+        Returns:
+        - None
+        """
         self.df = self.df.loc[(self.df['Time Datetime'] >= start_date) & (self.df['Time Datetime'] <= end_date)].copy()
         self.df.reset_index(inplace=True)
         daily_start_date = datetime.datetime(year=1900, month=1, day=1, hour=10, minute=0)
@@ -124,33 +165,50 @@ class AutocorrelationTools:
             self.plot_autocorrelations_without_pacf(arr_returns, nlags, confidence_level, title)
 
 
+
+
 class KurtosisTools:
 
     def __init__(self, df):
         self.df = df.copy()
 
+
     @staticmethod
-    def compute_log_returns(df, period, normalize):
+    def compute_log_returns(df, normalize):
+        """
+        Adjust the log-returns contained in the dataframe df, so as to exclude the first log-return of each day. Indeed, we
+        look at time series limited to trading days from 9:30am to 4:00pm, so that the first log-return is computed without
+        taking into account 'after-hours' trading activity. See https://arxiv.org/abs/2311.07738 for a similar methodology.
+
+        Parameters:
+        - df (pandas.core.frame.DataFrame): Dataframe containing the data of interest. Should contain the following columns:
+            'Date': The day of each bar as a string
+            'Log-returns': The log-returns
+        - normalize (bool): A boolean value indicating whether the log-returns should be normalized on a daily basis or not
+
+        Returns:
+        - An numpy.ndarray containing treated log-returns
+        """
         # Removing the first log-return of each day (since it was calculated using the price of the previous day)
-        column_name = 'Log-returns {}-min Period'.format(period)
-        df_one_period = df.loc[df[column_name].notnull()][['Time', 'Date', column_name]].copy()
-        df_one_period.reset_index(drop=True, inplace=True)
-        df_one_period.reset_index(inplace=True)
-        df_one_period.rename(columns={'index': 'Index'}, inplace=True)
-        list_indices = df_one_period[['Index', 'Time', 'Date', column_name]].groupby('Date').first()['Index'].to_list()
-        df_one_period.drop(index=list_indices, inplace=True)
-        df_one_period.drop(['Index', 'Time'], axis=1, inplace=True)
-        df_one_period.reset_index(drop=True, inplace=True)
-        
+        df.reset_index(inplace=True)
+        df.rename(columns={'index': 'Index'}, inplace=True)
+        list_indices = df.groupby('Date').first()['Index'].to_list()
+        df.drop(index=list_indices, inplace=True)
+        df.drop('Index', axis=1, inplace=True)
+        df.reset_index(drop=True, inplace=True)
         if normalize:
-            dict_daily_averages = df_one_period.groupby('Date').mean().to_dict()[column_name]
-            dict_daily_sd = df_one_period.groupby('Date').std().to_dict()[column_name]
-            return np.array(df_one_period.apply(lambda row: (row[column_name] - dict_daily_averages[row['Date']]) / dict_daily_sd[row['Date']], axis=1).to_list())
+            dict_daily_averages = df.groupby('Date').mean().to_dict()['Log-returns']
+            dict_daily_sd = df.groupby('Date').std().to_dict()['Log-returns']
+            return np.array(df.apply(lambda row: (row['Log-returns'] - dict_daily_averages[row['Date']]) / dict_daily_sd[row['Date']], axis=1).to_list())
         else:
-            return np.array(df_one_period[column_name].to_list())
+            return np.array(df['Log-returns'].to_list())
+
 
     @staticmethod
     def calculate_excess_kurtosis(arr_returns):
+        """
+        Calculate excess kurtosis
+        """
         mean_returns = np.mean(arr_returns)
         std_returns = np.std(arr_returns)
         
@@ -161,14 +219,29 @@ class KurtosisTools:
         kurtosis = numerator / denominator - 3
         return kurtosis
 
+
     def compute_kurtosis_between_two_dates(self, start_date, end_date, normalize):
+        """
+        Calculate the kurtosis between two dates based on the log-returns from 1-minute period to 60-minute period.
+
+        Parameters:
+        - start_date (datetime.datetime): The start date to be considered when calculating the kurtosis
+        - end_date (datetime.datetime): The end date to be considered when calculating the kurtosis
+        - normalize (bool): A boolean value indicating whether the log-returns should be normalized on a daily basis or not
+
+        Returns:
+        - An numpy.ndarray containing the kurtosis for the 60 different periods
+        """
         df_log_returns = self.df.loc[(self.df['Time Datetime'] > start_date) & (self.df['Time Datetime'] <= end_date)].copy()
         df_log_returns.reset_index(drop=True, inplace=True)
-        
         list_periods = list(range(1, 61))
         list_excess_kurtosis = []
         for period in list_periods:
-            arr_returns = self.compute_log_returns(df_log_returns, period, normalize)
+            column_name = 'Log-returns {}-min Period'.format(period)
+            df_temp = df_log_returns.loc[df_log_returns[column_name].notnull()][['Date', column_name]].copy()
+            df_temp.reset_index(drop=True, inplace=True)
+            df_temp.rename(columns={column_name: 'Log-returns'}, inplace=True)
+            arr_returns = self.compute_log_returns(df_temp, normalize)
             kurtosis = self.calculate_excess_kurtosis(arr_returns)
             list_excess_kurtosis.append(kurtosis)
         
@@ -176,6 +249,19 @@ class KurtosisTools:
 
 
     def compute_kurtosis_to_plot(self, start_date, end_date, frequency, normalize):
+        """
+        Calculate the kurtosis over the whole period and as average of subperiods using the method self.compute_kurtosis_between_two_dates
+
+        Parameters:
+        - start_date (datetime.datetime): The start date to be considered when calculating the kurtosis
+        - end_date (datetime.datetime): The end date to be considered when calculating the kurtosis
+        - frequency (int): The number of minutes to be considered for each subperiod
+        - normalize (bool): A boolean value indicating whether the log-returns should be normalized on a daily basis or not
+
+        Returns:
+        - arr_kurtosis_average: A numpy.ndarray containing the kurtosis calculated as average of subperiods
+        - arr_kurtosis_one_period: A numpy.ndarray containing the kurtosis calculated over the whole period
+        """
         list_dates = []
         
         i = self.df.loc[self.df['Time Datetime'] <= end_date].index[-1]
@@ -197,8 +283,9 @@ class KurtosisTools:
             arr_kurtosis_full = np.concatenate([arr_kurtosis_full, arr_kurtosis], axis=0)
         
         arr_kurtosis_average = np.mean(arr_kurtosis_full, axis=0)
-        arr_kurtosis_one_period = self.compute_kurtosis_between_two_dates(start_date, end_date, normalize)
-        return arr_kurtosis_average, arr_kurtosis_one_period.reshape(60)
+        arr_kurtosis_one_period = self.compute_kurtosis_between_two_dates(start_date, end_date, normalize).reshape(60)
+        return arr_kurtosis_average, arr_kurtosis_one_period
+
 
     @staticmethod
     def plot_kurtosis(arr_kurtosis_average_standard,
@@ -208,6 +295,21 @@ class KurtosisTools:
                       start_date,
                       end_date,
                       frequency):
+        """
+        Plot the kurtosis both with standard returns and normalized returns
+
+        Parameters:
+        - arr_kurtosis_average_standard: The kurtosis calculated as average of subperiods using standard returns
+        - arr_kurtosis_one_period_standard: The kurtosis calculated over the whole period using standard returns
+        - arr_kurtosis_average_normalized: The kurtosis calculated as average of subperiods using normalized returns
+        - arr_kurtosis_one_period_normalized: The kurtosis calculated over the whole period using normalized returns
+        - start_date (datetime.datetime): The start date to be considered when calculating the kurtosis
+        - end_date (datetime.datetime): The end date to be considered when calculating the kurtosis
+        - frequency (int): The number of minutes to be considered for each subperiod
+
+        Returns:
+        - None
+        """
         nb_days_per_period = str(np.round(frequency / 391, decimals=0)).split('.0')[0]
         fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
         
@@ -233,7 +335,19 @@ class KurtosisTools:
         fig.subplots_adjust(top=0.68)
         plt.show()
 
+
     def calculate_and_plot_kurtosis(self, start_date, end_date, frequency):
+        """
+        Calculate and plot the kurtosis both with standard returns and normalized returns
+
+        Parameters:
+        - start_date (datetime.datetime): The start date to be considered when calculating the kurtosis
+        - end_date (datetime.datetime): The end date to be considered when calculating the kurtosis
+        - frequency (int): The number of minutes to be considered for each subperiod
+
+        Returns:
+        - None
+        """
         arr_kurtosis_average_standard, arr_kurtosis_one_period_standard = self.compute_kurtosis_to_plot(start_date, end_date, frequency, False)
         arr_kurtosis_average_normalized, arr_kurtosis_one_period_normalized = self.compute_kurtosis_to_plot(start_date, end_date, frequency, True)
         self.plot_kurtosis(arr_kurtosis_average_standard,
