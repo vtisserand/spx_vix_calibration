116ad1593cdaa580082794add4db0b873f04cefd
Vivien Tisserand <vivien.tisserand@gmail.com>
2024-05-06 23:30:42 +0200

    feat: prices ivs toggling as pure utils functions as well

--- src/data.py
+++ src/data.py
@@ -15,12 +15,51 @@ vec_find_vol_rat = np.vectorize(
 # The idea is to flatten the 3D data (like when pivoting a table) and have arrays maturity, strikes, prices.
 
 
-def normal_pdf(x: float, mean: float=0.0, var: float=1.0):
+def normal_pdf(x: float, mean: float = 0.0, var: float = 1.0):
     """
     Returns the normal density evaluated at point x.
     """
     vol = np.sqrt(var)
-    return np.exp(-0.5*np.square((x-mean)/vol))/(vol*np.sqrt(2.0*np.pi))
+    return np.exp(-0.5 * np.square((x - mean) / vol)) / (vol * np.sqrt(2.0 * np.pi))
+
+
+def ivs_to_prices(
+    ivs: np.ndarray,
+    ttms: np.ndarray,
+    strikes: np.ndarray,
+    underlying: Union[float, np.ndarray] = 100.0,
+):
+    d_1 = (
+        (np.log(underlying / strikes) + ivs**2 / 2 * np.sqrt(ttms))
+        / ivs
+        * np.sqrt(ttms)
+    )
+    d_2 = d_1 - ivs * np.sqrt(ttms)
+    bs_prices = underlying * normal_pdf(d_1) - strikes * normal_pdf(d_2)
+    return bs_prices
+
+
+def prices_to_ivs(
+    prices: np.ndarray,
+    ttms: np.ndarray,
+    strikes: np.ndarray,
+    forwards: np.ndarray,
+    flags: np.ndarray,
+):
+    res = []
+    for i in range(len(prices)):
+        res.append(
+            vec_find_vol_rat(
+                price=prices[i],
+                S=forwards[i],
+                K=strikes[i],
+                t=ttms[i],
+                r=0,
+                flag=flags[i],
+            )
+        )
+    return res
+
 
 class OptionChain:
     def __init__(
@@ -29,7 +68,7 @@ class OptionChain:
         strikes: np.ndarray,
         forwards: np.ndarray,
         flags: np.ndarray,
-        underlying: Union[float, np.ndarray]=100.0,
+        underlying: Union[float, np.ndarray] = 100.0,
         ivs: Optional[np.ndarray] = None,
         prices: Optional[np.ndarray] = None,
         bid_ivs: Optional[np.ndarray] = None,
@@ -52,106 +91,149 @@ class OptionChain:
         elif prices is not None:
             self.prices = np.array(prices)
         else:
-            raise ValueError("Incomplete option chain: please provide prices or implied volatility for all instruments.")
+            raise ValueError(
+                "Incomplete option chain: please provide prices or implied volatility for all instruments."
+            )
 
     def get_iv(self):
         if self.ivs is not None:
             return self.ivs
         else:
             self.ivs = np.zeros_like(self.prices)
-            
+
             for i in range(len(self.prices)):
-                self.ivs[i] = vec_find_vol_rat(self.prices[i], self.forwards[i], self.strikes[i], self.ttms[i], 0, self.flags[i])
+                self.ivs[i] = vec_find_vol_rat(
+                    self.prices[i],
+                    self.forwards[i],
+                    self.strikes[i],
+                    self.ttms[i],
+                    0,
+                    self.flags[i],
+                )
 
         return self.ivs
-    
+
     def ivs_to_prices(self):
-        d_1 = (np.log(self.underlying / self.strikes) + self.ivs**2 / 2 * np.sqrt(self.ttms)) / self.ivs * np.sqrt(self.ttms)
+        d_1 = (
+            (
+                np.log(self.underlying / self.strikes)
+                + self.ivs**2 / 2 * np.sqrt(self.ttms)
+            )
+            / self.ivs
+            * np.sqrt(self.ttms)
+        )
         d_2 = d_1 - self.ivs * np.sqrt(self.ttms)
         bs_prices = self.underlying * normal_pdf(d_1) - self.strikes * normal_pdf(d_2)
         return bs_prices
 
-
     def prices_to_ivs(self):
         res = []
         for i in range(len(self.prices)):
-            res.append(vec_find_vol_rat(self.prices[i], self.forwards[i], self.strikes[i], self.ttms[i], 0, self.flags[i]))
+            res.append(
+                vec_find_vol_rat(
+                    self.prices[i],
+                    self.forwards[i],
+                    self.strikes[i],
+                    self.ttms[i],
+                    0,
+                    self.flags[i],
+                )
+            )
         return res
 
     def group_by_slice(self):
         slice_groups = defaultdict(lambda: {"strikes": [], "forwards": [], "flags": []})
-        
-        for ttm, strike, forward, flag in zip(self.ttms, self.strikes, self.forwards, self.flags):
+
+        for ttm, strike, forward, flag in zip(
+            self.ttms, self.strikes, self.forwards, self.flags
+        ):
             slice_groups[ttm]["strikes"].append(strike)
             slice_groups[ttm]["forwards"].append(forward)
             slice_groups[ttm]["flags"].append(flag)
-        
+
         for group in slice_groups.values():
             group["strikes"] = np.array(group["strikes"])
             group["forwards"] = np.array(group["forwards"])
             group["flags"] = np.array(group["flags"])
-            
+
         return slice_groups
-    
+
     def get_unique_ttms(self):
         """
         Returns an ordered list of the maturities for which we have data.
         """
         return sorted(set(self.ttms), key=lambda x: self.ttms.index(x))
-    
+
     def get_vegas(self):
         """
-        Return the chain vegas -- the Black Scholes greek at time t for a give maturity T is 
+        Return the chain vegas -- the Black Scholes greek at time t for a give maturity T is
         `S_t N'(d_1) \sqrt(T-t)`, where `d_1 = (\log(S_t/K) + (r + \sigma^2/2)(T-t)) / \sigma \sqrt(T-t)`
         and `N'(.)` is the standard normal pdf. Assumed `r=0`.
         """
-        d_1 = (np.log(self.underlying / self.strikes) + self.ivs**2 / 2 * np.sqrt(self.ttms)) / self.ivs * np.sqrt(self.ttms)
+        d_1 = (
+            (
+                np.log(self.underlying / self.strikes)
+                + self.ivs**2 / 2 * np.sqrt(self.ttms)
+            )
+            / self.ivs
+            * np.sqrt(self.ttms)
+        )
         return self.underlying * normal_pdf(d_1) * np.sqrt(self.ttms)
-    
+
     def plot_2d(self):
         """
         Scatter plot of the option surface assuming ivs are provided.
         """
         unique_ttms = np.unique(self.ttms)
-        color_map = plt.cm.get_cmap('rainbow', len(unique_ttms))
+        color_map = plt.cm.get_cmap("rainbow", len(unique_ttms))
 
         ttm_legend_added = {}
 
-        fig,ax=plt.subplots()
+        fig, ax = plt.subplots()
         for ttm, strike, iv in zip(self.ttms, self.strikes, self.ivs):
             color_index = np.where(unique_ttms == ttm)[0][0]
-            ax.scatter(strike, 100*iv, color=color_map(color_index), label=f'Maturity: {ttm}' if ttm not in ttm_legend_added else '')
+            ax.scatter(
+                strike,
+                100 * iv,
+                color=color_map(color_index),
+                label=f"Maturity: {ttm}" if ttm not in ttm_legend_added else "",
+            )
             ttm_legend_added[ttm] = True
 
-        ax.set_xlabel('Strikes')
-        ax.set_ylabel('IVs (in %)')
-        ax.set_title('2D visualization of the option chain')
+        ax.set_xlabel("Strikes")
+        ax.set_ylabel("IVs (in %)")
+        ax.set_title("2D visualization of the option chain")
         ax.legend()
 
         fig.show()
 
     def plot_3d(self):
         unique_ttms = np.unique(self.ttms)
-        color_map = plt.cm.get_cmap('rainbow', len(unique_ttms))
+        color_map = plt.cm.get_cmap("rainbow", len(unique_ttms))
 
         ttm_legend_added = {}
 
-        fig = plt.figure(figsize=(6,6))
-        ax = fig.add_subplot(111, projection='3d')
+        fig = plt.figure(figsize=(6, 6))
+        ax = fig.add_subplot(111, projection="3d")
 
         for ttm, strike, iv in zip(self.ttms, self.strikes, self.ivs):
             color_index = np.where(unique_ttms == ttm)[0][0]
-            ax.scatter(strike, ttm, 100*iv, color=color_map(color_index), label=f'Maturity: {ttm}' if ttm not in ttm_legend_added else '')
+            ax.scatter(
+                strike,
+                ttm,
+                100 * iv,
+                color=color_map(color_index),
+                label=f"Maturity: {ttm}" if ttm not in ttm_legend_added else "",
+            )
             ttm_legend_added[ttm] = True
 
-        ax.set_xlabel('Strikes')
-        ax.set_zlabel('IVs (in %)')
-        ax.set_ylabel('Time to Maturity')
-        ax.set_title('3D visualization of the option chain')
+        ax.set_xlabel("Strikes")
+        ax.set_zlabel("IVs (in %)")
+        ax.set_ylabel("Time to Maturity")
+        ax.set_title("3D visualization of the option chain")
         ax.legend()
 
         ax.view_init(elev=25, azim=-60)
         ax.dist = 11
 
         fig.show()
-
