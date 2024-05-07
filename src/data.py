fix: correct black scholes put formula

src/data.py
@@ -47,11 +47,12 @@ def ivs_to_prices(
     strikes: np.ndarray,
     underlying: Union[float, np.ndarray] = 100.0,
 ):
-    d_1 = (np.log(underlying / strikes) + (ivs ** 2 / 2) + ttms) / (ivs + np.sqrt(ttms))
+    """
+    For a put.
+    """
+    d_1 = (np.log(underlying / strikes) + (ivs ** 2 / 2) * ttms) / (ivs * np.sqrt(ttms))
     d_2 = d_1 - ivs * np.sqrt(ttms)
-    print(d_1)
-    print(d_2)
-    bs_prices = underlying * norm.cdf(d_1) - strikes * norm.cdf(d_2)
+    bs_prices = - underlying * norm.cdf(-d_1) + strikes * norm.cdf(-d_2)
     return bs_prices
 
 

src/models/qHeston.py
@@ -507,7 +507,7 @@ class qHeston(BaseModel):
 
             return error
 
-        market_prices = ivs_to_prices(ivs=option_chain.ivs, ttms=option_chain.ttms, strikes=option_chain.ttms, underlying=option_chain.underlying)
+        market_prices = ivs_to_prices(ivs=option_chain.ivs, ttms=option_chain.ttms, strikes=option_chain.strikes, underlying=option_chain.underlying)
         def objective_price_vega_weighted(params: np.ndarray, *args) -> float:
             print(f"params: {params}")
             self.set_parameters(*params)
@@ -625,7 +625,7 @@ class qHeston(BaseModel):
             )
 
         res = minimize(
-            objective_price_vega_weighted, init_guess, args=(option_chain,), method="nelder-mead", tol=10
+            objective_price_vega_weighted, init_guess, args=(option_chain,), method="nelder-mead",
         )
 
         return res.x, errs